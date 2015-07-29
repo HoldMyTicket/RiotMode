aabb54ba0a3813e7c0c879fa65b8410fcd1e9f47
Evan Fajardo <e.faj33@gmail.com>
2015-07-29 11:01:57 -0600

    shell script

--- load.sh
+++ load.sh
@@ -10,8 +10,9 @@ do
     cp $dir/src/*.tag $temp
 done
     
-echo "======================"
+echo "\n"
 echo "Compiling main dist..."    
+echo "\n"
 riot $temp dist/RiotMode.js
 rm -rf $temp
 
@@ -19,3 +20,20 @@ for i in `find mixins -name '*.js'` ;
 do 
     cat $i dist/RiotMode.js > tmp && mv tmp dist/RiotMode.js
 done
+
+ver=`git describe --abbrev=0 --tags`
+
+while true; do
+    read -p "You are on $ver, should we make a new one? (y/n): " yn
+    case $yn in
+        [Yy]* ) inc=true; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+if [ "$inc" = true ] ; then
+    read -p "Type version in the format v0.0.0: " ver
+    git tag $ver
+    git push origin $ver
+fi
