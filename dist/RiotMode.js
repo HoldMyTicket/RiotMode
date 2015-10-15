ef1f6c8170b2f6d60fc1568375b4f31f9cf09aad
Wes <wes@holdmyticket.com>
2015-10-15 00:47:43 -0600

    compiled

--- dist/RiotMode.js
+++ dist/RiotMode.js
@@ -511,7 +511,7 @@ riot.tag('raw', '<span></span>', function(opts) {
 	this.root.innerHTML = opts.content
 
 });
-riot.tag('rm-modal', '<div class="modalMaster" show="{open}"> <div class="overlay" onclick="{closeModal}"></div> <div class="modal"> <div class="modal-content"><yield></yield></div> <div class="clear"></div> </div> </div> <button onclick="{ openModal }" class="{ opts[\'open-btn-class\'] }"><i class="{ opts[\'open-btn-icon\'] }"></i> { opts[\'open-btn-text\'] }</button>', 'rm-modal .modalMaster { position: fixed; top: 0; right: 0; bottom: 0; left: 0; z-index: 100; } rm-modal .overlay { position: fixed; top: 0; right: 0; bottom: 0; left: 0; text-align: center; z-index: 101; background-color: rgba(0, 0, 0, 0.8); } rm-modal .modal { max-width: 35%; position: fixed; left: 33%; top: 20%; padding: 15px; background-color: #fff; z-index: 102; -webkit-border-radius: 5px; -moz-border-radius: 5px; -o-border-radius: 5px; border-radius: 5px; } rm-modal .affirmative-btn { float: left; } rm-modal .dismissive-btn { float: right; } rm-modal .clear { clear: both; } rm-modal .hidden { display: none; }', function(opts) {
+riot.tag('rm-modal', '<div class="modalMaster" show="{open}"> <div class="overlay" onclick="{closeModal}"></div> <div class="modal"> <div class="modal-content"><yield></yield></div> <div class="clear"></div> </div> </div> <button hide="{hide_btn}" onclick="{ openModal }" class="{ opts[\'open-btn-class\'] }"><i class="{ opts[\'open-btn-icon\'] }"></i> { opts[\'open-btn-text\'] }</button>', 'rm-modal .modalMaster { position: fixed; top: 0; right: 0; bottom: 0; left: 0; z-index: 100; } rm-modal .overlay { position: fixed; top: 0; right: 0; bottom: 0; left: 0; text-align: center; z-index: 101; background-color: rgba(0, 0, 0, 0.8); } rm-modal .modal { max-width: 35%; position: fixed; left: 33%; top: 20%; padding: 15px; background-color: #fff; z-index: 102; -webkit-border-radius: 5px; -moz-border-radius: 5px; -o-border-radius: 5px; border-radius: 5px; } rm-modal .affirmative-btn { float: left; } rm-modal .dismissive-btn { float: right; } rm-modal .clear { clear: both; } rm-modal .hidden { display: none; }', function(opts) {
     
     
     
@@ -522,6 +522,7 @@ riot.tag('rm-modal', '<div class="modalMaster" show="{open}"> <div class="overla
     this.affirmativeBtn = opts['confirm-btn'] == 'true' ? true : false;
     this.dismissiveBtn = opts['cancel-btn'] == 'true' ? true : false;
     this.open = false;
+    this.hide_btn = false;
     
     this.openModal = function(e) {
       this.open = true;
@@ -529,6 +530,8 @@ riot.tag('rm-modal', '<div class="modalMaster" show="{open}"> <div class="overla
       if(typeof RiotControl != 'undefined'){
         RiotControl.trigger('modalopened');
       }
+      if(!this.opts['open-btn-text'])
+        this.hide_btn = true;
       this.fire('open', e);
     }.bind(this);
     
@@ -542,11 +545,11 @@ riot.tag('rm-modal', '<div class="modalMaster" show="{open}"> <div class="overla
     }.bind(this);
     
     this.confirmBtn = function(e) {
-        opts.onconfirm();
+      opts.onconfirm();
     }.bind(this);
     
     this.cancelBtn = function(e) {
-        opts.oncancel();
+      opts.oncancel();
     }.bind(this);
     
 
